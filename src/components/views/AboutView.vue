77e7c1817ed50b76f5226ca484dec07d499f5661
Sam <samuelcourchesne2004@gmail.com>
2025-08-20 15:07:07 -0400

    feat: add construction message on none implemented page

--- src/components/views/AboutView.vue
+++ src/components/views/AboutView.vue
@@ -1,13 +1,19 @@
 <template>
     <main>
         <NavBar />
+        <section class="py-66 text-center">
+            <h1 class="text-5xl font-bold mb-4">🚧 Page Under Construction 🚧</h1>
+            <p class="text-xl text-gray-600">
+                We're working hard to bring this page to life. Please check back soon!
+            </p>
+        </section>
+        <Footer />
     </main>
 </template>
 
 <script setup>
 import NavBar from '../layout/NavBar.vue';
+import Footer from '../layout/Footer.vue';
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
