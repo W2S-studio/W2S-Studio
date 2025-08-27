b2f9b396afa6676c742aebe2a890bff88f198af8
Sam <samuelcourchesne2004@gmail.com>
2025-08-27 10:40:49 -0400

    feat: implement new Hero component and update localization for hero section

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-        <NavBar/>
-        <HeroSection />
+        <NavBar />
+        <Hero :text="{
+                title1: t('hero.line1'),
+                title2: t('hero.line2'),
+                title3: t('hero.line3'),
+                description: t('hero.description'),
+                button: t('hero.cta')
+        }" />
         <AboutSection />
         <ServiceSection />
         <ProjectSection />
@@ -11,13 +17,18 @@
 
 <script setup>
 import NavBar from '../layout/NavBar.vue';
-import HeroSection from '../sections/HeroSection.vue';
+import Hero from '../layout/Hero.vue'
 import AboutSection from '../sections/AboutSection.vue';
 import ContactSection from '../sections/ContactSection.vue';
 import ProjectSection from '../sections/ProjectSection.vue';
 import ServiceSection from '../sections/ServiceSection.vue';
 import TeamSection from '../sections/TeamSection.vue';
 import Footer from '../layout/Footer.vue';
+
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
 </script>
 
 <style lang="css" scoped></style>
